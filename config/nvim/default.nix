42a38cfa7eed32f8105616efea05e5cce08d2d20
James Atkins <code@jamesatkins.net>
2021-07-21 15:02:40 +0100

    Change neovim version number

--- config/nvim/default.nix
+++ config/nvim/default.nix
@@ -5,7 +5,7 @@ let
   neovimOverlay =
     final: prev: { 
       neovim-unwrapped = prev.neovim-unwrapped.overrideAttrs (oldAttrs: {
-        version = "master";
+        version = "0.5.0";
         src = builtins.fetchTarball {
           url = https://github.com/neovim/neovim/archive/refs/tags/v0.5.0.tar.gz;
           sha256 = "0lgbf90sbachdag1zm9pmnlbn35964l3khs27qy4462qzpqyi9fi";
