daaba164b4b9bf51bdb205c1bf6d02a07a974e9f
James Atkins <code@jamesatkins.net>
2021-07-27 11:46:51 +0100

    Add unstable nixpkgs to flake.nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:nixos/nixos-hardware";
 
     home-manager = {
@@ -9,25 +10,34 @@
     };
   };
 
-  outputs = { self, nixpkgs, nixos-hardware, home-manager }: rec {
+  outputs = { self, nixpkgs, nixpkgs-unstable, nixos-hardware, home-manager }: rec {
+
     nixosConfigurations.milan = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
 
-      modules = [
-        nixos-hardware.nixosModules.lenovo-thinkpad-t480
-        home-manager.nixosModules.home-manager
-        ./configuration.nix
-
-        {
-          system.configurationRevision = if self ? rev then self.rev else "dirty";
-
-          # Point nixpkgs to the nixpkgs used to build the system
-          nix = {
-            registry.nixpkgs.flake = nixpkgs;
-            nixPath = [ "nixpkgs=${nixpkgs.outPath}" ];
+      modules =
+        let
+          # Inject unstable packages into the package set
+          overlay-unstable = final: prev: rec {
+            unstable = import nixpkgs-unstable { system = final.system; };
           };
-        }
-      ];
+        in [
+          nixos-hardware.nixosModules.lenovo-thinkpad-t480
+          home-manager.nixosModules.home-manager
+          ./configuration.nix
+
+          {
+            system.configurationRevision = if self ? rev then self.rev else "dirty";
+
+            nixpkgs.overlays = [overlay-unstable ];
+
+            # Point nixpkgs to the nixpkgs used to build the system
+            nix = {
+              registry.nixpkgs.flake = nixpkgs;
+              nixPath = [ "nixpkgs=${nixpkgs.outPath}" ];
+            };
+          }
+        ];
     };
 
     githubActionsPkgs =
