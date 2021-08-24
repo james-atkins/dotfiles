b21380ecc49b1630ef74843d83bc60cf1bb325f8
James Atkins <code@jamesatkins.net>
2021-08-24 14:23:27 +0100

    Remove overlay for VSCode

--- config/vscode.nix
+++ config/vscode.nix
@@ -2,22 +2,17 @@
 
 let
   # Force vscode to use Wayland
-  # TODO: Only force wayland is running under wayland
-  vscodeWayland =
-    final: prev: {
-      vscode = prev.vscode.overrideAttrs (oldAttrs: {
-        buildInputs = oldAttrs.buildInputs or [] ++ [ pkgs.makeWrapper ];
-        postInstall = oldAttrs.postInstall or "" + ''
-          wrapProgram $out/bin/${prev.vscode.executableName} \
-            --add-flags "--enable-features=UseOzonePlatform --ozone-platform=wayland"
-        '';
-      });
-    };
+  vscodeWayland = pkgs.vscode.overrideAttrs (oldAttrs: {
+    buildInputs = oldAttrs.buildInputs or [] ++ [ pkgs.makeWrapper ];
+    postInstall = oldAttrs.postInstall or "" + ''
+      wrapProgram $out/bin/${pkgs.vscode.executableName} \
+        --add-flags "--enable-features=UseOzonePlatform --ozone-platform=wayland"
+    '';
+  });
 in
   {
     primary-user.home-manager.programs.vscode = {
       enable = true;
+      package = vscodeWayland;
     };
-
-    nixpkgs.overlays = [ vscodeWayland ];
   }
