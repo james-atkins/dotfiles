90d8c16a47fb22e1770664a2650c07699b78d43a
James Atkins <code@jamesatkins.net>
2021-09-06 18:44:12 +0100

    Rust: Add rustfmt and gcc

--- config/rust.nix
+++ config/rust.nix
@@ -2,14 +2,14 @@
 
 let
   rustStable = pkgs.rust-bin.stable.latest.default.override {
-    extensions = [ "rust-src" "rls" ];
+    extensions = [ "rust-src" "rls" "rustfmt" ];
   };
 in
   {
     nixpkgs.overlays = [ rust-overlay ];
 
     primary-user.home-manager = {
-      home.packages = [ rustStable ];
+      home.packages = with pkgs; [ rustStable gcc ];
 
       home.sessionVariables = {
         RUST_SRC_PATH = "${rustStable}/lib/rustlib/src/rust";
