6096ca33a7daec0de7f454e8d5ddba2d34ffd0c1
James Atkins <code@jamesatkins.net>
2022-02-28 21:30:07 +0000

    Add Anki

--- config/desktop/applications.nix
+++ config/desktop/applications.nix
@@ -2,12 +2,16 @@
 
 {
   primary-user.home-manager = {
-
     programs.firefox = { 
       enable = true;
     };
 
+    home.sessionVariables = {
+      ANKI_WAYLAND = 1;
+    };
+
     home.packages = with pkgs; [ 
+      anki
       evince
       skype
       teams
