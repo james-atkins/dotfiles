4cfea9428152f9c6113f0ec4852c1127b8b2f51a
James Atkins <code@jamesatkins.net>
2022-04-12 00:32:42 +0100

    Use vscode 1.65.2
    Latest version is broken on wayland.

--- config/vscode.nix
+++ config/vscode.nix
@@ -1,8 +1,17 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 let
   # Force vscode to use Wayland
-  vscodeWayland = pkgs.vscode.overrideAttrs (oldAttrs: {
+  vscodeWayland = pkgs.vscode.overrideAttrs (oldAttrs: rec {
+    # 1.66 is currently currently broken so use previous version
+    # https://github.com/microsoft/vscode/issues/146349
+    version = "1.65.2";
+    src = pkgs.fetchurl {
+      name = "VSCode_${version}_linux-x64.tar.gz";
+      url = "https://update.code.visualstudio.com/${version}/linux-x64/stable";
+      sha256 = "sha256-RNkbiZL07YSUHQZvRgN8BVLYIZgFiciPObYOJJ9hG3U=";
+    };
+
     buildInputs = oldAttrs.buildInputs or [] ++ [ pkgs.makeWrapper ];
     postInstall = oldAttrs.postInstall or "" + ''
       wrapProgram $out/bin/${pkgs.vscode.executableName} \
