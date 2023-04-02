502cd35ad9128e1948877db689582fdf3437be48
James Atkins <code@jamesatkins.net>
2023-04-02 11:47:13 -0500

    Fix nixpkgs-unstable URL
    Follow the unstable channel not the master branch

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:nixos/nixos-hardware";
     home-manager = {
       url = "github:nix-community/home-manager/release-22.11";
