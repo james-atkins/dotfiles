0182544c241764a4e3ebacd38ede86aa097fe663
James Atkins <code@jamesatkins.net>
2023-07-01 07:16:46 -0500

    Restart Tailscale on wake from sleep
    Workaround until tailscale/tailscale#8223 is fixed.

--- modules/tailscale.nix
+++ modules/tailscale.nix
@@ -16,4 +16,17 @@
 
   services.openssh.enable = lib.mkDefault true;
   programs.mosh.enable = lib.mkDefault true;
+
+  # Work-around this very annoying Tailscale bug by restarting Tailscale on resume from sleep
+  # https://github.com/tailscale/tailscale/issues/8223#issuecomment-1592176312
+  systemd.services.tailscale-restart = {
+    description = "Restart Tailscale after suspend";
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "systemctl restart tailscaled.service";
+    };
+    # https://github.com/systemd/systemd/issues/6364#issuecomment-316647050
+    wantedBy = [ "sleep.target" ];
+    after = [ "systemd-suspend.service" "systemd-hybrid-sleep.service" "systemd-hibernate.service" ];
+  };
 }
