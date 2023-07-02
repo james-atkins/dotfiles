55e06f7a18d1830df77fe0fd77fa4a4763ddc584
James Atkins <code@jamesatkins.net>
2023-07-02 15:13:01 -0500

    Change paperless mediadir

--- machines/zeus/paperless.nix
+++ machines/zeus/paperless.nix
@@ -1,9 +1,13 @@
 { config, pkgs, ... }:
 
+let
+  mediaDir = "/tank/paperless";
+in
 {
   age.secrets.paperless.file = ../../secrets/paperless.age;
 
   services.paperless = {
+    inherit mediaDir;
     enable = true;
     dataDir = "/persist/var/lib/paperless";
     address = "localhost";
@@ -12,6 +16,12 @@
       PAPERLESS_AUTO_LOGIN_USERNAME = "admin";
     };
   };
+
+  systemd.services.paperless-scheduler.unitConfig.RequiresMountsFor = [ mediaDir ];
+  systemd.services.paperless-task-queue.unitConfig.RequiresMountsFor = [ mediaDir ];
+  systemd.services.paperless-download-nltk-data.unitConfig.RequiresMountsFor = [ mediaDir ];
+  systemd.services.paperless-consumer.unitConfig.RequiresMountsFor = [ mediaDir ];
+  systemd.services.paperless-web.unitConfig.RequiresMountsFor = [ mediaDir ];
 
   ja.private-services.paperless.caddy-config = ''
     reverse_proxy http://127.0.0.1:${toString config.services.paperless.port}
