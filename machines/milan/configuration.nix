4804f671751c6fe0b46dca2755ae51825c971c49
James Atkins <code@jamesatkins.net>
2023-07-12 06:48:08 -0500

    Add extra scanner backend

--- machines/milan/configuration.nix
+++ machines/milan/configuration.nix
@@ -60,7 +60,10 @@
   # ES-60W scanner
   hardware.sane = {
     enable = true;
-    extraBackends = [ (pkgs-local.epsonscan2.override { withNonFreePlugins = true; }) ];
+    extraBackends = [
+      pkgs.hplipWithPlugin
+      (pkgs-local.epsonscan2.override { withNonFreePlugins = true; })
+    ];
     disabledDefaultBackends = [ "epsonds" ];
   };
   home-manager.users.james.home.packages = [ pkgs.simple-scan ];
