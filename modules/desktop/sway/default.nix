3a92725331375bb84f4a628cef515cfb395aa97b
James Atkins <code@jamesatkins.net>
2023-09-07 12:57:32 -0500

    sway: update extra session commands

--- modules/desktop/sway/default.nix
+++ modules/desktop/sway/default.nix
@@ -123,10 +123,14 @@ lib.mkIf config.ja.desktop.enable {
         include ~/.config/sway/sway.config
       '';
       extraSessionCommands = ''
-        export XDG_SESSION_TYPE=wayland
-        export XDG_CURRENT_DESKTOP=sway
+        export SDL_VIDEODRIVER=wayland
+
+        # needs qt5.qtwayland
+        export QT_QPA_PLATFORM=wayland
         export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
-        export MOZ_ENABLE_WAYLAND=1
+
+        # Fix for some Java AWT applications (e.g. Android Studio),
+        # use this if they aren't displayed properly:
         export _JAVA_AWT_WM_NONREPARENTING=1
       '';
     };
