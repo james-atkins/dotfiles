273030d8768bf4e4b75fe1b8131342c64bdca64b
James Atkins <code@jamesatkins.net>
2023-12-08 19:54:50 -0600

    vendorSha256 -> vendorHash

--- pkgs/borgmatic-zfs-snapshot/default.nix
+++ pkgs/borgmatic-zfs-snapshot/default.nix
@@ -3,7 +3,7 @@
 buildGoModule rec {
   name = "borgmatic-zfs-snapshot";
   src = ./.;
-  vendorSha256 = "sha256-G5uPkndn7h3CTHWtwPnAqo6pxVhNxbtbEMv67gdqpTM=";
+  vendorHash = "sha256-G5uPkndn7h3CTHWtwPnAqo6pxVhNxbtbEMv67gdqpTM=";
   postPatch = ''
     substituteInPlace main.go --replace '@BORGMATIC@' ${borgmatic}/bin/borgmatic
   '';
