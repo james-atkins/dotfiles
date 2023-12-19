a2df73e89ec6a15365430ed0033774d1a423992a
James Atkins <code@jamesatkins.net>
2023-12-19 23:31:39 +0000

    Register tailscale as ephemeral node

--- modules/tailscale.nix
+++ modules/tailscale.nix
@@ -75,15 +75,16 @@ in
       ];
       network.ssh.authorizedKeys = config.users.users.james.openssh.authorizedKeys.keys;
       network.postCommands = lib.mkBefore ''
-        mkdir -p /var/lib/tailscale
-        nohup /bin/.tailscaled-wrapped -verbose=1 -state=/var/lib/tailscale/tailscaled.state -no-logs-no-support -socket ./tailscaled.socket &
-        /bin/.tailscale-wrapped --socket=./tailscaled.socket up --hostname=${config.networking.hostName}-boot --auth-key=file:/etc/tailscale.secret
+        mkdir -p /var/run/tailscale
 
-        echo "#!/bin/sh" >> ./logout.sh
-        echo "/bin/.tailscale-wrapped --socket=./tailscaled.socket logout; killall zfs" >> ./logout.sh
-        chmod +x ./logout.sh
+        nohup /bin/.tailscaled-wrapped -verbose=1 -state=mem: -no-logs-no-support &
+        /bin/.tailscale-wrapped up --hostname=${config.networking.hostName}-boot --auth-key=file:/etc/tailscale.secret
 
-        echo "zpool import -a; zfs load-key -a; (( ./logout.sh > /dev/null 2> /dev/null < /dev/null & ) & ); exit" >> /root/.profile
+        echo "#!/bin/sh" >> /root/logout.sh
+        echo "/bin/.tailscale-wrapped logout; killall zfs" >> /root/logout.sh
+        chmod +x /root/logout.sh
+
+        echo "zpool import -a; zfs load-key -a; (( /root/logout.sh > /dev/null 2> /dev/null < /dev/null & ) & ); exit" >> /root/.profile
       '';
     };
   };
