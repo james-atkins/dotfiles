9482d2d23a2fdba90f0b3e6ef394bc535f56a598
James Atkins <code@jamesatkins.net>
2024-01-21 21:22:19 -0600

    Switch to full texlive

--- modules/desktop/latex.nix
+++ modules/desktop/latex.nix
@@ -1,14 +1,9 @@
 { config, lib, pkgs, ... }:
 
-let
-  tex = pkgs.texlive.combine {
-    inherit (pkgs.texlive) scheme-medium chngcntr csquotes enumitem xpatch;
-  };
-in
 lib.mkIf config.ja.desktop.enable {
   home-manager.users.james = { pkgs, ... }: {
     home.packages = with pkgs; [
-      tex
+      texlive.combined.scheme-full
 
       lyx
       kile
